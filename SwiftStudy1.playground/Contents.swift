3d3d8cdf526204667307b8ce7a4834e8d24b6403
Pavan <pavan.more1@gmail.com>
2024-09-09 02:34:38 +0530

    Largest number in an array

--- SwiftStudy1.playground/Contents.swift
+++ SwiftStudy1.playground/Contents.swift
@@ -12,3 +12,26 @@ class NetworkManager {
 
 let singletonInstance = NetworkManager.shared
 print(singletonInstance);
+
+
+// Write program to find largest number from dictionary
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+
+
+func largestNumber() {
+    var largestNum = 0
+    for (_, numbers) in interestingNumbers {
+        for number in numbers {
+            if(number > largestNum) {
+               largestNum = number
+            }
+        }
+    }
+    print("Largest: \(largestNum)")
+}
+
+largestNumber()
